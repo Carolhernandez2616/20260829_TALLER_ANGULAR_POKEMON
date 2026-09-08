a63615e6837023ba3e18cd418ae3e0a1629dbb9b
CarolLaMasPlay <cyhc33454638@soy.sena.edu.co>
2026-09-08 02:06:32 +0000

    Cambios

--- BuscadorPokemon/.angular/cache/22.1.7/pokedex/vite/deps/@angular_router.js
+++ BuscadorPokemon/.angular/cache/22.1.7/pokedex/vite/deps/@angular_router.js
@@ -1,6 +1,9 @@
-import { $n as Output, Bl as Subject, Bt as computed, Dc as InjectionToken, Di as provideAppInitializer, Dl as ɵɵdefineInjectable, Ea as ɵɵcontentQuery, Ec as INTERNAL_APPLICATION_ERROR_HANDLER, Ei as performanceMarkFeature, En as ElementRef, Er as ViewContainerRef, Fn as Injectable, Gl as identity, Hc as PendingTasksInternal, Hl as operate, Ic as NgZone, In as Input, Jc as Version, Jo as ɵɵinvalidFactory, Lo as ɵɵinjectAttribute, Mi as publishNonCoreGlobalUtil, Mn as IS_HYDRATION_DOM_REUSE_ENABLED, Mr as afterNextRender, O as booleanAttribute, Oc as Injector, Ol as ɵɵdefineInjector, Ps as ɵɵsanitizeUrlOrResourceUrl, Qc as assertInInjectionContext, Rl as map, Sl as signal, Ul as Observable, Vl as createOperatorSubscriber, Wc as RuntimeError, Wi as setClassMetadata, Wl as pipe, Wt as linkedSignal, X as input, Xl as createErrorClass, Xo as ɵɵloadQuery, Yl as Subscription, Yn as NgModuleFactory$1, Yo as ɵɵlistener, Yt as APP_BOOTSTRAP_LISTENER, Zl as isFunction$1, _c as ENVIRONMENT_INITIALIZER, a as ContentChildren, an as ChangeDetectionStrategy, ao as ɵɵdefineService, au as __values, bl as runInInjectionContext, cl as inject, cn as Component, dr as Service, et as maybeUnwrapDefaultExport, f as HostAttributeToken, fl as isStandalone, fn as Console, ft as reflectComponentType, gc as DestroyRef, gl as promiseWithResolvers, go as ɵɵelement, hc as DOCUMENT, ir as Renderer2, iu as __spreadArray, jl as ɵɵinject, jn as IS_ENABLED_BLOCKING_INITIAL_NAVIGATION, kn as HostListener, la as ɵɵNgOnChangesFeature, lc as _asyncToGenerator, lu as _objectSpread2, ml as makeEnvironmentProviders, nl as effect, nn as Attribute, no as ɵɵdefineDirective, on as Compiler, oo as ɵɵdirectiveInject, pt as resourceFromSnapshots, ql as noop$1, qn as NgModule, qr as createEnvironmentInjector, qt as untracked, r as ChangeDetectorRef, rl as formatRuntimeError, ro as ɵɵdefineNgModule, ru as __read, tn as ApplicationRef, to as ɵɵdefineComponent, ul as isInjectable, uu as _defineProperty, vi as isNgModule, vl as provideEnvironmentInitializer, vs as ɵɵqueryRefresh, wn as Directive, xc as EventEmitter, ya as ɵɵattribute, yc as EnvironmentInjector, yi as isPromise, zl as BehaviorSubject } from "./core-CJxV53BA.js";
-import { $ as of, J as switchMap, Q as mergeMap, U as LOCATION_INITIALIZED, W as PlatformLocation, X as concatMap, Y as finalize, Z as filter, et as from, it as popScheduler, nt as innerFrom, rt as popResultSelector, tt as executeSchedule } from "./http-BSRvWmsW.js";
-import { B as LocationStrategy, F as ViewportScroller, I as PRECOMMIT_HANDLER_SUPPORTED, L as PlatformNavigation, P as NavigationAdapterForLocation, R as HashLocationStrategy, V as PathLocationStrategy, s as Title, z as Location } from "./platform-browser-BuMhFfUh.js";
+import { $n as Output, Bl as Subject, Bt as computed, Dc as InjectionToken, Di as provideAppInitializer, Dl as ɵɵdefineInjectable, Ea as ɵɵcontentQuery, Ec as INTERNAL_APPLICATION_ERROR_HANDLER, Ei as performanceMarkFeature, En as ElementRef, Er as ViewContainerRef, Fn as Injectable, Gl as identity, Hc as PendingTasksInternal, Hl as operate, Ic as NgZone, In as Input, Jc as Version, Jo as ɵɵinvalidFactory, Lo as ɵɵinjectAttribute, Mi as publishNonCoreGlobalUtil, Mn as IS_HYDRATION_DOM_REUSE_ENABLED, Mr as afterNextRender, O as booleanAttribute, Oc as Injector, Ol as ɵɵdefineInjector, Ps as ɵɵsanitizeUrlOrResourceUrl, Qc as assertInInjectionContext, Rl as map, Sl as signal, Ul as Observable, Vl as createOperatorSubscriber, Wc as RuntimeError, Wi as setClassMetadata, Wl as pipe, Wt as linkedSignal, X as input, Xl as createErrorClass, Xo as ɵɵloadQuery, Yl as Subscription, Yn as NgModuleFactory$1, Yo as ɵɵlistener, Yt as APP_BOOTSTRAP_LISTENER, Zl as isFunction$1, _c as ENVIRONMENT_INITIALIZER, a as ContentChildren, an as ChangeDetectionStrategy, ao as ɵɵdefineService, au as __values, bl as runInInjectionContext, cl as inject, cn as Component, dr as Service, et as maybeUnwrapDefaultExport, f as HostAttributeToken, fl as isStandalone, fn as Console, ft as reflectComponentType, gc as DestroyRef, gl as promiseWithResolvers, go as ɵɵelement, hc as DOCUMENT, ir as Renderer2, jl as ɵɵinject, jn as IS_ENABLED_BLOCKING_INITIAL_NAVIGATION, kn as HostListener, la as ɵɵNgOnChangesFeature, lc as _asyncToGenerator, lu as _objectSpread2, ml as makeEnvironmentProviders, nl as effect, nn as Attribute, no as ɵɵdefineDirective, on as Compiler, oo as ɵɵdirectiveInject, pt as resourceFromSnapshots, ql as noop$1, qn as NgModule, qr as createEnvironmentInjector, qt as untracked, r as ChangeDetectorRef, rl as formatRuntimeError, ro as ɵɵdefineNgModule, tn as ApplicationRef, to as ɵɵdefineComponent, ul as isInjectable, uu as _defineProperty, vi as isNgModule, vl as provideEnvironmentInitializer, vs as ɵɵqueryRefresh, wn as Directive, xc as EventEmitter, ya as ɵɵattribute, yc as EnvironmentInjector, yi as isPromise, zl as BehaviorSubject } from "./core-CJxV53BA.js";
+import { c as innerFrom, l as popResultSelector, n as LOCATION_INITIALIZED, o as from, r as PlatformLocation, s as executeSchedule, u as popScheduler } from "./_platform_location-chunk-UfsGc9CU.js";
+import { G as concatMap, J as of, K as filter, U as switchMap, W as finalize, q as mergeMap } from "./http-BdJ9Srus.js";
+import { n as argsArgArrayOrObject, r as mapOneOrManyArgs, t as createObject } from "./createObject-C3MVIb8m.js";
+import { a as PlatformNavigation, c as LocationStrategy, i as PRECOMMIT_HANDLER_SUPPORTED, l as PathLocationStrategy, r as ViewportScroller, s as Location, t as NavigationAdapterForLocation } from "./common-B8QzsFlx.js";
+import { P as HashLocationStrategy, s as Title } from "./platform-browser-CkLW2COd.js";
 //#region node_modules/.pnpm/rxjs@7.8.2/node_modules/rxjs/dist/esm5/internal/observable/empty.js
 var EMPTY = new Observable(function(subscriber) {
 	return subscriber.complete();
@@ -32,55 +35,6 @@ var EmptyError = createErrorClass(function(_super) {
 		this.message = "no elements in sequence";
 	};
 });
-//#endregion
-//#region node_modules/.pnpm/rxjs@7.8.2/node_modules/rxjs/dist/esm5/internal/util/mapOneOrManyArgs.js
-var isArray$1 = Array.isArray;
-function callOrApply(fn, args) {
-	return isArray$1(args) ? fn.apply(void 0, __spreadArray([], __read(args))) : fn(args);
-}
-function mapOneOrManyArgs(fn) {
-	return map(function(args) {
-		return callOrApply(fn, args);
-	});
-}
-//#endregion
-//#region node_modules/.pnpm/rxjs@7.8.2/node_modules/rxjs/dist/esm5/internal/util/argsArgArrayOrObject.js
-var isArray = Array.isArray;
-var getPrototypeOf = Object.getPrototypeOf;
-var objectProto = Object.prototype;
-var getKeys = Object.keys;
-function argsArgArrayOrObject(args) {
-	if (args.length === 1) {
-		var first_1 = args[0];
-		if (isArray(first_1)) return {
-			args: first_1,
-			keys: null
-		};
-		if (isPOJO(first_1)) {
-			var keys = getKeys(first_1);
-			return {
-				args: keys.map(function(key) {
-					return first_1[key];
-				}),
-				keys
-			};
-		}
-	}
-	return {
-		args,
-		keys: null
-	};
-}
-function isPOJO(obj) {
-	return obj && typeof obj === "object" && getPrototypeOf(obj) === objectProto;
-}
-//#endregion
-//#region node_modules/.pnpm/rxjs@7.8.2/node_modules/rxjs/dist/esm5/internal/util/createObject.js
-function createObject(keys, values) {
-	return keys.reduce(function(result, key, i) {
-		return result[key] = values[i], result;
-	}, {});
-}
 //#endregion
 //#region node_modules/.pnpm/rxjs@7.8.2/node_modules/rxjs/dist/esm5/internal/observable/combineLatest.js
 function combineLatest() {
--- BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.ts
+++ BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.ts
@@ -1,9 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+export interface Usuario {
+    id : number;
+    nombreCompleto : string ;
+    documento : {
+        tipo: string;
+        numero: string;
+},
+    fecha_Nacimiento : string;
+    Genero : string;
+    Correo_electronico : string;
+    Numero_de_telefono : string ;
+    domicilio : {
+        Pais_de_domicilio : string;
+        Ciudad_de_domicilio : string;
+    },
+    tratamiento_de_datos :boolean;
+}
+
+
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-registro-usuario',
+  standalone: true, // DECLARA QUE UN COMPONENTE ES AUTONOMO
   styleUrl: './registro-usuario.css',
   templateUrl: './registro-usuario.html',
 })
-export class RegistroUsuario {}
+export class RegistroUsuario {
+ Nombre = signal('');
+ Apellido = signal('');
+ Tipo_de_documento = signal('CC');
+ dni = signal('');
+ Fecha_de_nacimiento = signal('');
+ Genero = signal('');
+ Correo_electronico = signal('');
+ Numero_de_telefono = signal('');
+ Pais_de_domicilio = signal('');
+ Ciudad_de_domicilio = signal('');
+ Tratamiento_de_datos = signal(false);
+
+ ultimoUsuario = signal<Usuario | null>(null);
+
+ guardarUsuario() {
+    if (!this.Tratamiento_de_datos()) {
+        alert('Debe aceptar el tratamiento de datos para continuar.');
+    }
+const usuarioCreado = {
+    id : Date.now(),
+    nombreCompleto : `${this.Nombre()} ${this.Apellido()}`,
+    documento : {
+        tipo: this.Tipo_de_documento(),
+        numero: this.dni()
+},
+    fecha_Nacimiento : this.Fecha_de_nacimiento(),
+    Genero : this.Genero(),
+    Correo_electronico : this.Correo_electronico(),
+    Numero_de_telefono : this.Numero_de_telefono(),
+    domicilio : {
+        Pais_de_domicilio : this.Pais_de_domicilio(),
+        Ciudad_de_domicilio : this.Ciudad_de_domicilio()
+    },
+    tratamiento_de_datos : this.Tratamiento_de_datos(),
+}
+
+//Guardar los datos en el navegador
+
+//localStorage.setItem('crearUsuario',usuarioJSON)estatico
+//dinamico
+//Guardarlo en una sola linea de codigo
+localStorage.setItem(usuarioCreado.id.toString(), JSON.stringify(usuarioCreado));
+this.ultimoUsuario.set(usuarioCreado);
+  }
+}  //Crear una funcion Crear el usuario
+
