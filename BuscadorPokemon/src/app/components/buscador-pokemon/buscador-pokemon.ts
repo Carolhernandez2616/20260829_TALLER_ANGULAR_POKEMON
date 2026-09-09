a2d67b326bae6e7d8b5e64019440d43c52520f50
CarolLaMasPlay <cyhc33454638@soy.sena.edu.co>
2026-09-09 01:18:01 +0000

    Creación de página web Pokemon

--- BuscadorPokemon/src/app/components/buscador-pokemon/buscador-pokemon.ts
+++ BuscadorPokemon/src/app/components/buscador-pokemon/buscador-pokemon.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface PokemonData {
+  name: string;
+  image: string;
+  type: string;
+}
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
+  standalone: true, // DECLARA QUE UN COMPONENTE ES AUTONOMO
   selector: 'app-buscador-pokemon',
   styleUrl: './buscador-pokemon.css',
   templateUrl: './buscador-pokemon.html',
 })
-export class BuscadorPokemon {}
+
+
+export class BuscadorPokemon {
+  nombrePokemonInput = signal('') ;
+  pokemon = signal<PokemonData | null>(null);
+  mensajeError = signal<string | null>(null);
+
+  async buscarPokemon() {
+  
+  const nombrePokemon = this.nombrePokemonInput().trim().toLowerCase();
+
+  if (!nombrePokemon) return;
+
+  this.mensajeError.set(null); // Limpiar mensaje de error antes de la búsqueda
+  
+  try {
+    
+    const  respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
+
+    if (!respuesta.ok) {
+    throw  new Error('¡No se encontró el Pokémon!');
+    }
+
+    const datos = await respuesta.json();
+
+    this.pokemon.set({
+      name: datos.name.toUpperCase(),
+      image: datos.sprites.front_default,
+      type: datos.types.map((typeInfo: any) => typeInfo.type.name).join (', '), //EL MAP ME CONVIERTE EN TEXTO DE ACUERDO CON LA PROPIEDAD QUE YO LE PIDA EN ESTE CASO ES NAME//ME LLAMA LISTAS
+    });
+
+  }catch (error: any) {
+    this.pokemon.set(null);
+    this.mensajeError.set(error.message);
+  }
+  
+}
+
+}
