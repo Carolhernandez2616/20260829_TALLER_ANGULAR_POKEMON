029c93fad9c75b5f5eec633ff885a4c8c06d1da6
CarolLaMasPlay <cyhc33454638@soy.sena.edu.co>
2026-09-10 01:42:16 +0000

    Creación del CRUD

--- BuscadorPokemon/src/app/services/pokemon-storage.ts
+++ BuscadorPokemon/src/app/services/pokemon-storage.ts
@@ -12,7 +12,7 @@ export interface PokemonTarjeta {
 @Injectable({
     providedIn: 'root'
 
-    })
+    })//cualquier elemento o objeto puede ingresar al servicio
 
 export class PokemonStorageService {
     private http = inject(HttpClient);
@@ -26,8 +26,41 @@ export class PokemonStorageService {
 
 private cargarDesdeStorage() {
     const data = localStorage.getItem(this.storageKey);
+
     if (data) {
         this.misPokemons.set(JSON.parse(data));
     }
 }
+//1. Obtener datos de la API de Pokémon
+buscarEnAPI(nombreOId: string) {
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${nombreOId.toLowerCase}`);
+
 }
+//2. Guardar/Crear nuevo Pokémon en el almacenamiento local
+guardarPokemon(nuevo: PokemonTarjeta) {
+    const actualizados = [...this.misPokemons(), nuevo];
+    this.misPokemons.set(actualizados);//es agregar un nuevo valor 
+    localStorage.setItem(this.storageKey, JSON.stringify(actualizados));
+}
+//3.Actualizar Pokémon favorito
+actualizarFavorito(id: number) {
+    const actualizados = this.misPokemons().map(poke => {
+        if (poke.id === id) {
+            return { ...poke, esFavorito: !poke.esFavorito };
+}
+return poke;
+    });
+
+    this.misPokemons.set(actualizados);
+    localStorage.setItem(this.storageKey, JSON.stringify(actualizados));
+};
+
+
+//4. Eliminar Pokémon 
+eliminarPokemon(id: number) {
+    const filtrados = this.misPokemons().filter(poke => poke.id !== id);//FILTER,
+    this.misPokemons.set(filtrados);
+    localStorage.setItem(this.storageKey, JSON.stringify(filtrados));
+}
+}
+
