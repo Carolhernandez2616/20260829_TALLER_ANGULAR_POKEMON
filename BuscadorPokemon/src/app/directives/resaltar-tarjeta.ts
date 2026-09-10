ce8cf393f786fb8ac2063aa5c7fb9d7b8f36d74e
CarolLaMasPlay <cyhc33454638@soy.sena.edu.co>
2026-09-10 02:04:35 +0000

    Creación de las Directivas y Servicios

---
+++ BuscadorPokemon/src/app/directives/resaltar-tarjeta.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener,inject ,input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltarTarjeta]',
+  standalone: true
+})
+export class ResaltarTarjeta {
+  private el = inject(ElementRef);
+  colorBorde = input<string>('#FFFF00');
+  
+  @HostListener('mouseenter') onMouseEnter() {
+    this.aplicarEfecto(`3px solid ${this.colorBorde()}`, 'scale(1.03)');
+}
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.aplicarEfecto(`3px solid #55D48F`, 'scale(1)');
+}
+
+private aplicarEfecto(borde: string, transformacion: string) {
+    this.el.nativeElement.style.border = borde;
+    this.el.nativeElement.style.transform = transformacion;
+    this.el.nativeElement.style.transition = 'all 0.3s ease-in-out';
+  }
+}
